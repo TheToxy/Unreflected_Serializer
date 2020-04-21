4c26fa870adbe09d6bd9a84c27c6969cf71bae89
Ondrej Kraus <ondrakraus@seznam.cz>
2020-04-22 00:39:40 +0200

    Generic version of GetDescriptor

--- UnreflectedSerializer/Program.cs
+++ UnreflectedSerializer/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace UnreflectedSerializer
@@ -16,7 +13,10 @@ namespace UnreflectedSerializer
         public static string EncapsulateElement(string attribute, bool closing = false)
         {
             if (closing)
+            {
                 return start + close + attribute + end;
+            }
+
             return start + attribute + end;
         }
 
@@ -77,7 +77,7 @@ namespace UnreflectedSerializer
     {
         static void Main(string[] args)
         {
-            RootDescriptor<Person> rootDesc = GetPersonDescriptor();
+            RootDescriptor<Person> rootDesc = GetGenericDescriptor<Person>("Person", FieldDescriptor.person);
 
             var czechRepublic = new Country { Name = "Czech Republic", AreaCode = 420 };
             var person = new Person
@@ -92,93 +92,44 @@ namespace UnreflectedSerializer
 
             rootDesc.Serialize(Console.Out, person);
         }
-        static class PersonDescriptor{
-            public static readonly Dictionary<string, Action<Person, TextWriter, string>> fields = new Dictionary<string, Action<Person, TextWriter, string>>
-            {
-                { "FirstName", (person, writter, fieldName) => XML.SerializeElement(fieldName, person.FirstName, writter)},
-                { "LastName" , (person, writter, fieldName) => XML.SerializeElement(fieldName, person.LastName, writter) },            
-                { "HomeAddress", (p,w,fName) => GetAddressDescriptor(fName).Serialize(w, p.HomeAddress)},
-                { "WorkAddress" , (p,w,fName) => GetAddressDescriptor(fName).Serialize(w, p.WorkAddress)},
-                { "CitizenOf" , (p,w,fName) => GetCountryDescriptor(fName).Serialize(w, p.CitizenOf)},
-                { "MobilePhone" , (p,w,fName) => GetPhoneNumberDescriptor(fName).Serialize(w, p.MobilePhone)}
-            };
-        }
-
-        static class AddressDescriptor
+        static class FieldDescriptor
         {
-            public static readonly Dictionary<string, Action<Address, string, TextWriter>> fields = new Dictionary<string, Action<Address, string, TextWriter>>
+            public static readonly Dictionary<string, Action<Person, string, TextWriter>> person = new Dictionary<string, Action<Person, string, TextWriter>>
+            {
+                { "FirstName", (person, fieldName, writer) => XML.SerializeElement(fieldName, person.FirstName, writer)},
+                { "LastName" , (person, fieldName, writer) => XML.SerializeElement(fieldName, person.LastName, writer) },
+                { "HomeAddress", (person, fieldName, writer) => GetGenericDescriptor(fieldName, address).Serialize(writer, person.HomeAddress)},
+                { "WorkAddress" , (person, fieldName, writer) => GetGenericDescriptor(fieldName, address).Serialize(writer, person.WorkAddress)},
+                { "CitizenOf" , (person, fieldName, writer) => GetGenericDescriptor(fieldName, country).Serialize(writer, person.CitizenOf)},
+                { "MobilePhone" , (person, fieldName, writer) => GetGenericDescriptor(fieldName, phoneNumber).Serialize(writer, person.MobilePhone)},
+            };
+
+            public static readonly Dictionary<string, Action<Address, string, TextWriter>> address = new Dictionary<string, Action<Address, string, TextWriter>>
             {
                 { "Street" ,  (address, fName, writter) => XML.SerializeElement(fName, address.Street, writter) },
                 { "City", (address, fName, writter) => XML.SerializeElement(fName, address.City, writter) },
-            };          
-        }
+            };
 
-        static class CountryDescriptor
-        {
-            public static readonly Dictionary<string, Action<Country, string, TextWriter>> fields = new Dictionary<string, Action<Country, string, TextWriter>>
+            public static readonly Dictionary<string, Action<Country, string, TextWriter>> country = new Dictionary<string, Action<Country, string, TextWriter>>
             {
                 { "Name", (country, fName, writter) => XML.SerializeElement(fName, country.Name, writter) },
                 { "AreaCode" ,  (country, fName, writter) => XML.SerializeElement(fName, country.AreaCode, writter) }
             };
-        }
-
-        static class PhoneNumberDescriptor
-        {
-            public static readonly Dictionary<string, Action<PhoneNumber, string, TextWriter>> fields = new Dictionary<string, Action<PhoneNumber, string, TextWriter>>
+            public static readonly Dictionary<string, Action<PhoneNumber, string, TextWriter>> phoneNumber = new Dictionary<string, Action<PhoneNumber, string, TextWriter>>
             {
-                { "Country", (phoneNum, fName, writer) => GetCountryDescriptor(fName).Serialize(writer, phoneNum.Country) },
+                { "Country", (phoneNum, fName, writer) => GetGenericDescriptor(fName, country).Serialize(writer, phoneNum.Country) },
                 { "Number" , (phoneNum, fName, writer) => XML.SerializeElement(fName, phoneNum.Number, writer) }
             };
         }
 
-        static RootDescriptor<Person> GetPersonDescriptor(string elementName = "Person")
-        {          
-            var rootDesc = new RootDescriptor<Person>(elementName);
-            rootDesc.actions = (Person person, TextWriter writter) =>
-            {                
-                foreach (var pair in PersonDescriptor.fields)
-                {
-                    pair.Value(person, writter, pair.Key);
-                }              
-            };
-            return rootDesc;
-        }
-
-        static RootDescriptor<Address> GetAddressDescriptor(string elementName = "Address")
+        static RootDescriptor<T> GetGenericDescriptor<T>(string elementName, Dictionary<string, Action<T, string, TextWriter>> fieldDescriptor)
         {
-            var rootDesc = new RootDescriptor<Address>(elementName);
-            rootDesc.actions = (Address address, TextWriter writter) =>
+            var rootDesc = new RootDescriptor<T>(elementName);
+            rootDesc.actions = (T instance, TextWriter writer) =>
             {
-                foreach (var pair in AddressDescriptor.fields)
+                foreach (var pair in fieldDescriptor)
                 {
-                    pair.Value(address, pair.Key, writter);
-                }
-            };
-
-            return rootDesc;
-        }
-       
-        static RootDescriptor<Country> GetCountryDescriptor(string elementName = "Country")
-        {
-            var rootDesc = new RootDescriptor<Country>(elementName);
-            rootDesc.actions = (Country country, TextWriter writter) =>
-            {
-                foreach (var pair in CountryDescriptor.fields)
-                {
-                    pair.Value(country, pair.Key, writter);
-                }
-            };
-            return rootDesc;
-        }
-
-        static RootDescriptor<PhoneNumber> GetPhoneNumberDescriptor(string elementName = "Country")
-        {
-            var rootDesc = new RootDescriptor<PhoneNumber>(elementName);
-            rootDesc.actions = (PhoneNumber phoneNumber, TextWriter writter) =>
-            {
-                foreach (var pair in PhoneNumberDescriptor.fields)
-                {
-                    pair.Value(phoneNumber, pair.Key, writter);
+                    pair.Value(instance, pair.Key, writer);
                 }
             };
             return rootDesc;
